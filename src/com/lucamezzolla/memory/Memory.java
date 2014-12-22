87a3f0f6846383147dc70cfb8765c5ed7b61f8c6
lucamezzolla <lucamezzolla@gmail.com>
2014-12-22 20:18:31 +0100

    Configuration.

--- src/com/lucamezzolla/memory/Memory.java
+++ src/com/lucamezzolla/memory/Memory.java
@@ -1,6 +1,9 @@
 package com.lucamezzolla.memory;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  *
@@ -13,10 +16,25 @@ public class Memory {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        MainFrame mf = new MainFrame();
+        new Memory().TimerProp();
+    	MainFrame mf = new MainFrame();
         mf.setResizable(false);
         mf.setLocationRelativeTo(null);
         mf.setVisible(true);
+    }
+    
+    public void TimerProp() throws FileNotFoundException, IOException {
+    	Properties propTimer = new Properties();
+		propTimer.setProperty("easyWait", "5");
+        propTimer.setProperty("mediumWait", "10");
+        propTimer.setProperty("hardWait", "60");
+        propTimer.setProperty("easyTimer", "10");
+        propTimer.setProperty("mediumTimer", "30");
+        propTimer.setProperty("hardTimer", "60");
+        FileOutputStream fos = new FileOutputStream("timer.properties");
+        propTimer.store(fos, null);
+        fos.flush();
+        fos.close();
     }
     
 }
